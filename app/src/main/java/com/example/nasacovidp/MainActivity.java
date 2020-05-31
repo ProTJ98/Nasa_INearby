93af1aa5b8ddbf87fbd4916c720b47e5a06825f5
ProTJ98 <64562280+ProTJ98@users.noreply.github.com>
2020-05-31 11:51:05 +0100

    V1.3

--- app/src/main/java/com/example/nasacovidp/MainActivity.java
+++ app/src/main/java/com/example/nasacovidp/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.nasacovidp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1,btn2,btn3,btn4,btn5,btn6;
+    Context context;
+
+
+    String CHANNEL_ID = "notification";
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        btn1 = findViewById(R.id.btn1);
+
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new activity_band(), false, "one" );
+            }
+        });
+
+
+
+
+    }
+
+    public void addFragment(Fragment fragment, boolean addToBackStack, String tag){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if(addToBackStack){
+            ft.addToBackStack(tag);
+        }
+        ft.replace(R.id.container,fragment,tag);
+        ft.commitAllowingStateLoss();
+
     }
 }
---
+++ app/src/main/java/com/example/nasacovidp/activity_band.java
@@ -0,0 +1,16 @@
+package com.example.nasacovidp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class activity_band extends Fragment {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    private void setContentView(int activity_profile) {
+    }
+
+}
